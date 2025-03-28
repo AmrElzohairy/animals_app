2858b81c4299313c03b3a077e14cfab29f10a63c
amrElzohairy <aelzohairy9@gmail.com>
2025-03-28 22:05:56 +0200

    add animals text

--- lib/features/onboarding/home/presentation/ui/widgets/home_carosal_item.dart
+++ lib/features/onboarding/home/presentation/ui/widgets/home_carosal_item.dart
@@ -2,13 +2,28 @@ import 'package:flutter/material.dart';
 
 class HomeCarosalItem extends StatelessWidget {
   const HomeCarosalItem({super.key, required this.image});
-final String image;
+  final String image;
   @override
   Widget build(BuildContext context) {
     return ClipRRect(
       borderRadius: BorderRadius.circular(10),
-      child: Image.asset(image, // Use the image passed to the constructor
-          width: double.infinity, fit: BoxFit.cover),
+      child: Stack(
+        children: [
+          Image.asset(
+            image, // Use the image passed to the constructor
+            width: double.infinity,
+            fit: BoxFit.cover,
+          ),
+          Positioned(
+            bottom: 10,
+            left: 10,
+            child: Text(
+              "Animals",
+              style: TextStyle(color: Colors.white, fontSize: 24),
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
